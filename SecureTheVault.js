0c911183d19e2d1304cafe7dd6aada988c35ea12
Tyler <tbos92@gmail.com>
2024-07-30 21:28:02 -0400

    secure the vault final

---
+++ SecureTheVault.js
@@ -0,0 +1,13 @@
+/* 
+Create a variable of type string that states "You have received this message because 
+you have been chosen to open an important vault. Here is the secret combination: "
+*/
+const string = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+
+// Assign 3 variables using 3 different artihmentic functions equaling the 3 combination values of 10, 40, and 39
+const combo1 = 20-10;
+const combo2 = 400/10;
+const combo3 = 13*3;
+
+// create an alert dialog box displaying the string text and complete combination
+alert(`${string}${combo1}-${combo2}-${combo3}`)
